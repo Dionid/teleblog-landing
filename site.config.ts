6007119b1054d9d198808a8ad4bfc49b7dcf18e4
David Shek <ditreyw@gmail.com>
2023-03-04 18:05:31 +0000

    feat: css changes

--- site.config.ts
+++ site.config.ts
@@ -11,7 +11,7 @@ export default siteConfig({
   // basic site info (required)
   name: 'Next.js Notion Starter Kit',
   domain: 'fop.davidshekunts.ru',
-  author: 'David Shekunts',
+  author: '👨🏻 David Shekunts',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
