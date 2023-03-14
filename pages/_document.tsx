3550ca340aef72d3e6675711d46435b0e8fc2550
David Shek <ditreyw@gmail.com>
2023-03-14 23:23:28 +0000

    fix: yandex

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -78,8 +78,6 @@ export default class MyDocument extends Document {
             <script
               dangerouslySetInnerHTML={{
                 __html: `
-<!-- Yandex.Metrika counter -->
-<script type="text/javascript" >
   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
   m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
@@ -90,9 +88,13 @@ export default class MyDocument extends Document {
         accurateTrackBounce:true,
         webvisor:true
   });
-</script>
-<noscript><div><img src="https://mc.yandex.ru/watch/86342837" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
-<!-- /Yandex.Metrika counter -->
+`
+              }}
+            />
+            <noscript
+              dangerouslySetInnerHTML={{
+                __html: `
+                <div><img src="https://mc.yandex.ru/watch/86342837" style="position:absolute; left:-9999px;" alt="" /></div>
 `
               }}
             />
