d619d2d27b29a0eeff8d20a125ad977a7a4a2ea4
David Shek <ditreyw@gmail.com>
2025-06-21 21:13:29 +0800

    feat: ya metriks

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,29 +9,91 @@ export default class MyDocument extends Document {
       <IconContext.Provider value={{ style: { verticalAlign: 'middle' } }}>
         <Html lang='en'>
           <Head>
-            <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png"/>
-            <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png"/>
-            <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png"/>
-            <link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png"/>
-            <link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png"/>
-            <link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png"/>
-            <link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png"/>
-            <link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png"/>
-            <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png"/>
-            <link rel="icon" type="image/png" sizes="192x192"  href="/android-icon-192x192.png"/>
-            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
-            <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png"/>
-            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
-            <link rel="manifest" href="/manifest.json"/>
-            <meta name="msapplication-TileColor" content="#ffffff"/>
-            <meta name="msapplication-TileImage" content="/ms-icon-144x144.png"/>
-            <meta name="theme-color" content="#ffffff"/>
+            <link
+              rel='apple-touch-icon'
+              sizes='57x57'
+              href='/apple-icon-57x57.png'
+            />
+            <link
+              rel='apple-touch-icon'
+              sizes='60x60'
+              href='/apple-icon-60x60.png'
+            />
+            <link
+              rel='apple-touch-icon'
+              sizes='72x72'
+              href='/apple-icon-72x72.png'
+            />
+            <link
+              rel='apple-touch-icon'
+              sizes='76x76'
+              href='/apple-icon-76x76.png'
+            />
+            <link
+              rel='apple-touch-icon'
+              sizes='114x114'
+              href='/apple-icon-114x114.png'
+            />
+            <link
+              rel='apple-touch-icon'
+              sizes='120x120'
+              href='/apple-icon-120x120.png'
+            />
+            <link
+              rel='apple-touch-icon'
+              sizes='144x144'
+              href='/apple-icon-144x144.png'
+            />
+            <link
+              rel='apple-touch-icon'
+              sizes='152x152'
+              href='/apple-icon-152x152.png'
+            />
+            <link
+              rel='apple-touch-icon'
+              sizes='180x180'
+              href='/apple-icon-180x180.png'
+            />
+            <link
+              rel='icon'
+              type='image/png'
+              sizes='192x192'
+              href='/android-icon-192x192.png'
+            />
+            <link
+              rel='icon'
+              type='image/png'
+              sizes='32x32'
+              href='/favicon-32x32.png'
+            />
+            <link
+              rel='icon'
+              type='image/png'
+              sizes='96x96'
+              href='/favicon-96x96.png'
+            />
+            <link
+              rel='icon'
+              type='image/png'
+              sizes='16x16'
+              href='/favicon-16x16.png'
+            />
+            <link rel='manifest' href='/manifest.json' />
+            <meta name='msapplication-TileColor' content='#ffffff' />
+            <meta
+              name='msapplication-TileImage'
+              content='/ms-icon-144x144.png'
+            />
+            <meta name='theme-color' content='#ffffff' />
 
             <link rel='manifest' href='/manifest.json' />
 
-            <link rel="preconnect" href="https://fonts.googleapis.com"/>
-            <link rel="preconnect" href="https://fonts.gstatic.com"/>
-            <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet"/>
+            <link rel='preconnect' href='https://fonts.googleapis.com' />
+            <link rel='preconnect' href='https://fonts.gstatic.com' />
+            <link
+              href='https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap'
+              rel='stylesheet'
+            />
           </Head>
 
           <body>
@@ -79,6 +141,34 @@ export default class MyDocument extends Document {
 
             <NextScript />
           </body>
+          <script
+            type='text/javascript'
+            dangerouslySetInnerHTML={{
+              __html: `
+            (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();
+            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+            ym(102627198, "init", {
+                  clickmap:true,
+                  trackLinks:true,
+                  accurateTrackBounce:true,
+                  webvisor:true
+            });
+            `
+            }}
+          />
+          <noscript>
+            <div>
+              <img
+                src='https://mc.yandex.ru/watch/102627198'
+                style={{ position: 'absolute', left: '-9999px;' }}
+                alt=''
+              />
+            </div>
+          </noscript>
         </Html>
       </IconContext.Provider>
     )
